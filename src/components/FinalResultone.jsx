8fbefe0a90836ff27be4051b56390c8686be0ea2
ShubhChitkara <186718633+ShubhChitkara@users.noreply.github.com.>
2025-02-18 14:37:22 +0000

    increased font size

--- src/components/FinalResultone.jsx
+++ src/components/FinalResultone.jsx
@@ -21,16 +21,16 @@ function Resultone({
       <h4>Email-ID: {email}</h4>
       <h4>Phone Number: {phone}</h4>
       <h4>School-Name: {schoolname}</h4>
-      <h5>Degree-Name: {degreename}</h5>
-      <h6>
+      <h4>Degree-Name: {degreename}</h4>
+      <h4>
         Study-Timeline: {startofstudy}   to   {endofstudy}
-      </h6>
+      </h4>
       <h4>Company-Name: {companyname}</h4>
-      <h5>Position-Title: {positiontitle}</h5>
-      <h6>Responsibilties: {responsibilities}</h6>
-      <h6>
+      <h4>Position-Title: {positiontitle}</h4>
+      <h4>Responsibilties: {responsibilities}</h4>
+      <h4>
         Work-Timeline: {startofwork}   to   {endofwork}
-      </h6>
+      </h4>
       <button className="edit" onClick={editbtn}>
         Edit
       </button>
